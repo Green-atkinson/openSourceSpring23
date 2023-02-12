713efef4186f9363c0252c4d650b509f77a7f626
John Atkinson <johnatkinson2020@gmail.com>
2023-02-12 02:07:56 -0500

    Created react hello world app

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,7 +1,5 @@
-import Navbar from './components/Navbar/Navbar';
 import logo from './logo.svg';
 import './App.css';
-
 
 function App() {
   return (
@@ -9,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
